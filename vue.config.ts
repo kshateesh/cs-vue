ec01b6cf9b8a889df94a6a0f72c0ce382fa8f6ce
kshateesh <kshateesh007@gmail.com>
2024-02-28 15:03:52 +0530

    add mappings

--- vue.config.ts
+++ vue.config.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from '@vue/cli-service'
 
+//map envs
+(function () {
+  process.env.VUE_APP_CONTENTSTACK_API_KEY = process.env.CONTENTSTACK_API_KEY
+  process.env.VUE_APP_CONTENTSTACK_API_HOST = process.env.CONTENTSTACK_API_HOST
+  process.env.VUE_APP_CONTENTSTACK_DELIVERY_TOKEN = process.env.CONTENTSTACK_DELIVERY_TOKEN
+  process.env.VUE_APP_CONTENTSTACK_ENVIRONMENT = process.env.CONTENTSTACK_ENVIRONMENT
+  process.env.VUE_APP_CONTENTSTACK_CDN = process.env.CONTENTSTACK_CDN
+}());
+
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
